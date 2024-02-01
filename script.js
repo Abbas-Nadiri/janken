286db23e5c3a6ab44a9d71b5341e86780123d9f2
Abbas Nadiri <108028111+Abbas-Nadiri@users.noreply.github.com>
2024-02-01 15:25:00 +0000

    create method to catch invalid inputs

--- script.js
+++ script.js
@@ -4,4 +4,21 @@ function randomChoice(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
 
-console.log(randomChoice(aiArray));
+let userChoice = prompt("Make your choice: rock, paper, or scissors?");
+userChoice = userChoice.toLowerCase();
+console.log(`User choice: ${userChoice}`);
+console.log(`Computer choice: ${randomChoice(aiArray)}`);
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        alert("Draw!")
+    }
+}
+
+if (userChoice !== "paper" && userChoice !== "scissors" && userChoice !== "rock") {
+    alert("Please enter a valid choice. Reload to play again.")
+
+} else {
+
+    playRound(userChoice, randomChoice)
+}
